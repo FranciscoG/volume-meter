f4ef080b54eebe742de50d5b9d4c566eca8b5091
franciscog <frankjg@gmail.com>
2014-08-27 23:26:33 -0400

    got the arc meter sort of working

--- main.js
+++ main.js
@@ -21,70 +21,106 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+
+var log = function() {
+  return console.log(Array.prototype.slice.call(arguments));
+};
+
 var audioContext = null;
+var canvas = null;
 var meter = null;
 var canvasContext = null;
-var WIDTH=500;
-var HEIGHT=50;
+var WIDTH = 500;
+var HEIGHT = 500;
 var rafID = null;
+var x, y;
+// size of the circle
+var radius = 100;
 
 window.onload = function() {
 
-    // grab our canvas
-	canvasContext = document.getElementById( "meter" ).getContext("2d");
-	
-    // monkeypatch Web Audio
-    window.AudioContext = window.AudioContext || window.webkitAudioContext;
-	
-    // grab an audio context
-    audioContext = new AudioContext();
+  canvas = document.getElementById("meter");
 
-    // Attempt to get audio input
-    try {
-        // monkeypatch getUserMedia
-        navigator.getUserMedia = 
-        	navigator.getUserMedia ||
-        	navigator.webkitGetUserMedia ||
-        	navigator.mozGetUserMedia;
+  // get exact center coordinates
+  x = canvas.width / 2;
+  y = canvas.height / 2;
 
-        // ask for an audio input
-        navigator.getUserMedia({audio:true}, gotStream, didntGetStream);
-    } catch (e) {
-        alert('getUserMedia threw exception :' + e);
-    }
+  // grab our canvas
+  canvasContext = canvas.getContext("2d");
 
-}
+  // monkeypatch Web Audio
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+  // grab an audio context
+  audioContext = new AudioContext();
+
+  // Attempt to get audio input
+  try {
+    // monkeypatch getUserMedia
+    navigator.getUserMedia =
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia;
+
+    // ask for an audio input
+    navigator.getUserMedia({
+      audio: true
+    }, gotStream, didntGetStream);
+  } catch (e) {
+    alert('getUserMedia threw exception :' + e);
+  }
+
+};
 
 
 function didntGetStream() {
-    alert('Stream generation failed.');
+  alert('Stream generation failed.');
 }
 
 function gotStream(stream) {
-    // Create an AudioNode from the stream.
-    var mediaStreamSource = audioContext.createMediaStreamSource(stream);
+  // Create an AudioNode from the stream.
+  var mediaStreamSource = audioContext.createMediaStreamSource(stream);
 
-    // Create a new volume meter and connect it.
-	meter = createAudioMeter(audioContext);
-	mediaStreamSource.connect(meter);
+  // Create a new volume meter and connect it.
+  meter = createAudioMeter(audioContext);
+  mediaStreamSource.connect(meter);
 
-    // kick off the visual updating
-    drawLoop();
+  // kick off the visual updating
+  drawLoop();
 }
 
-function drawLoop( time ) {
-    // clear the background
-    canvasContext.clearRect(0,0,WIDTH,HEIGHT);
 
-    // check if we're currently clipping
-    if (meter.checkClipping())
-        canvasContext.fillStyle = "red";
-    else
-        canvasContext.fillStyle = "green";
 
-    // draw a bar based on the current volume
-    canvasContext.fillRect(0, 0, meter.volume*WIDTH*1.4, HEIGHT);
+function drawLoop(time) {
 
-    // set up the next visual callback
-    rafID = window.requestAnimationFrame( drawLoop );
+  //canvasContext.clearRect(0, 0, WIDTH, HEIGHT);
+
+  //canvasContext.globalCompositeOperation = 'destination-out';
+  canvasContext.beginPath();
+  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+  canvasContext.closePath();
+
+  // check if we're currently clipping
+  if (meter.checkClipping()) {
+    canvasContext.strokeStyle = "red";
+  } else {
+    canvasContext.strokeStyle = "blue";
+  }
+
+
+  // clear the background
+  var startAngle = 0.5 * Math.PI;
+  var endAngle = ((meter.volume * 1.5) * Math.PI) - 4;
+  //log(meter.volume, endAngle);
+
+  canvasContext.beginPath();
+  canvasContext.arc(x, y, radius, startAngle, endAngle, false);
+  canvasContext.lineWidth = 20;
+  canvasContext.stroke();
+
+  // draw a bar based on the current volume
+  // canvasContext.fillRect(0, 0, meter.volume * WIDTH * 1.4, HEIGHT);
+
+  // set up the next visual callback
+  rafID = window.requestAnimationFrame(drawLoop);
 }
